68a5b0d376adf1a814e1fb8f58247d88b2f07c2f
Mikhail Fediukin <90192527+Mikhail-Fediukin@users.noreply.github.com>
2023-10-09 21:56:27 +0300

    Add files via upload

---
+++ homework07/friends.py
@@ -0,0 +1,129 @@
+import dataclasses
+import time
+import typing as tp
+
+from vkapi import config
+from vkapi.session import Session
+
+QueryParams = tp.Optional[tp.Dict[str, tp.Union[str, int]]]
+
+
+@dataclasses.dataclass(frozen=True)
+class FriendsResponse:
+    count: int
+    items: tp.Union[tp.List[int], tp.List[tp.Dict[str, tp.Any]]]
+
+
+def get_friends(
+    user_id: int, count: int = 5000, offset: int = 0, fields: tp.Optional[tp.List[str]] = None
+) -> FriendsResponse:
+    """
+    Получить список идентификаторов друзей пользователя или расширенную информацию
+    о друзьях пользователя (при использовании параметра fields).
+
+    :param user_id: Идентификатор пользователя, список друзей для которого нужно получить.
+    :param count: Количество друзей, которое нужно вернуть.
+    :param offset: Смещение, необходимое для выборки определенного подмножества друзей.
+    :param fields: Список полей, которые нужно получить для каждого пользователя.
+    :return: Список идентификаторов друзей пользователя или список пользователей.
+    """
+    start = Session(config.VK_CONFIG["domain"])
+    resp = FriendsResponse(0, [0])
+    try:
+        friends = start.get(
+            "friends.get",
+            params={
+                "access_token": config.VK_CONFIG["access_token"],
+                "v": config.VK_CONFIG["version"],
+                "user_id": user_id,
+                "count": count,
+                "offset": offset,
+                "fields": fields,
+            },
+        )
+        resp = FriendsResponse(
+            friends.json()["response"]["count"], friends.json()["response"]["items"]
+        )
+    except:
+        pass
+    return resp
+
+
+class MutualFriends(tp.TypedDict):
+    id: int
+    common_friends: tp.List[int]
+    common_count: int
+
+
+def get_mutual(
+    source_uid: tp.Optional[int] = None,
+    target_uid: tp.Optional[int] = None,
+    target_uids: tp.Optional[tp.List[int]] = None,
+    order: str = "",
+    count: tp.Optional[int] = None,
+    offset: int = 0,
+    progress=None,
+) -> tp.Union[tp.List[int], tp.List[MutualFriends]]:
+    """
+    Получить список идентификаторов общих друзей между парой пользователей.
+
+    :param source_uid: Идентификатор пользователя, чьи друзья пересекаются с друзьями пользователя с идентификатором target_uid.
+    :param target_uid: Идентификатор пользователя, с которым необходимо искать общих друзей.
+    :param target_uids: Cписок идентификаторов пользователей, с которыми необходимо искать общих друзей.
+    :param order: Порядок, в котором нужно вернуть список общих друзей.
+    :param count: Количество общих друзей, которое нужно вернуть.
+    :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
+    :param progress: Callback для отображения прогресса.
+    """
+    start = Session(config.VK_CONFIG["domain"])
+    all_friends = []
+    if target_uids:
+        for i in range(((len(target_uids) - 1) // 100) + 1):
+            try:
+                mutual_friends = start.get(
+                    "friends.getMutual",
+                    params={
+                        "access_token": config.VK_CONFIG["access_token"],
+                        "v": config.VK_CONFIG["version"],
+                        "source_uid": source_uid,
+                        "target_uid": target_uid,
+                        "target_uids": ",".join(list(map(str, target_uids))),
+                        "order": order,
+                        "count": 100,
+                        "offset": i * 100,
+                    },
+                )
+                for friend in mutual_friends.json()["response"]:
+                    all_friends.append(
+                        MutualFriends(
+                            id=friend["id"],
+                            common_friends=list(map(int, friend["common_friends"])),
+                            common_count=friend["common_count"],
+                        )
+                    )
+            except:
+                pass
+        return all_friends
+    try:
+        mutual_friends = start.get(
+            "friends.getMutual",
+            params={
+                "access_token": config.VK_CONFIG["access_token"],
+                "v": config.VK_CONFIG["version"],
+                "source_uid": source_uid,
+                "target_uid": target_uid,
+                "target_uids": target_uids,
+                "order": order,
+                "count": count,
+                "offset": offset,
+            },
+        )
+        all_friends.extend(mutual_friends.json()["response"])
+    except:
+        pass
+    return all_friends
+
+
+if __name__ == "__main__":
+    print(get_friends(178052017).items)
+    print(get_mutual(178052017, 213294079))
---
+++ homework07/session.py
@@ -0,0 +1,44 @@
+import typing as tp
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry  # type: ignore
+
+
+class Session:
+    """
+    Сессия.
+
+    :param base_url: Базовый адрес, на который будут выполняться запросы.
+    :param timeout: Максимальное время ожидания ответа от сервера.
+    :param max_retries: Максимальное число повторных запросов.
+    :param backoff_factor: Коэффициент экспоненциального нарастания задержки.
+    """
+
+    def __init__(
+        self,
+        base_url: str,
+        timeout: float = 5.0,
+        max_retries: int = 3,
+        backoff_factor: float = 0.3,
+    ) -> None:
+        self.base_url = base_url
+        self.timeout = timeout
+        self.session = requests.Session()
+        self.retry_strategy = Retry(
+            total=max_retries,
+            backoff_factor=backoff_factor,
+            method_whitelist=["GET", "POST"],
+            status_forcelist=list(range(400, 600)),
+        )
+        self.adapter = HTTPAdapter(max_retries=self.retry_strategy)
+        self.session.mount(base_url, self.adapter)
+
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        kwargs["timeout"] = kwargs["timeout"] if "timeout" in kwargs else self.timeout
+        response = self.session.get(self.base_url + "/" + url, *args, **kwargs)
+        return response
+
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        kwargs["timeout"] = kwargs["timeout"] if "timeout" in kwargs else self.timeout
+        response = self.session.post(self.base_url + "/" + url, *args, **kwargs)
+        return response
